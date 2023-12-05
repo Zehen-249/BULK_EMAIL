258f34cec6be75b7fcb0195f7f9c9624971aa529
zehen-249 <1hasanmehendi123@gmail.com>
2023-12-05 22:55:40 +0530

    5th nov

--- Sponsor.py
+++ Sponsor.py
@@ -13,35 +13,57 @@ password = passwd
 for i in target:
     em = MIMEMultipart() 
 
-    subject = 'ROVER PROJECT: Kirori Mal College, University of Delhi'
+    subject = 'ROVER PROJECT: Kirori Mal College,University of Delhi'
     em['From'] = sender
     em['Subject'] = subject
     context = ssl.create_default_context()
 
+#     body = """Dear {},
+
+# I hope this email finds you well. My name is Ravikant Verma, and I am writing on behalf of the Robotics Society of Kirori Mal College, Delhi University. As passionate students, we have been actively involved in the development of Robotics Application such as Rovers. Our recent accomplishment includes showcasing our innovative Infinit-Rover 2 in front of Prime Minister Shri Narendra Modi and Education Minister Dharmendra Pradhan during the Valedictory Ceremony of Centenary Celebration of University of Delhi and now we are aiming to move a step ahead and participate in International Rover Competitions with our Upgraded Infinit-Rover 3.
+
+# Our Rover Project involves designing and building a cutting-edge rover to compete in international challenges. This initiative not only enhances the skills of our student members but also contributes to the overall advancement of science and technology.
+
+# We are reaching out to you because we believe {} shares our commitment to fostering innovation and education. The Rover Project aligns with our mission to push the boundaries of technological advancements and inspire future generations. To achieve our goals, we are seeking financial support and sponsorship from organizations that recognize the value of investing in education and technological innovation.
+
+# We understand the importance of a symbiotic relationship, and we are open to discussing customized sponsorship packages that align with your corporate objectives.
+
+# If you are interested in supporting our project, we would be grateful to arrange a meeting at your convenience to further discuss the details. We are confident that this partnership will not only contribute to the success of our Rover Project but also showcase your company as a supporter of innovation and education.
+
+
+# Best Regards,
+
+# Ravikant Verma
+# Project Chief
+# Robotics Society of Kirori Mal College, University of Delhi
+# +91 788 074 9927
+# """.format(i.name,i.company)
     body = """Dear {},
 
-I hope this email finds you well. My name is Ravikant Verma, and I am writing on behalf of the Robotics Society of Kirori Mal College, Delhi University. As passionate students, we have been actively involved in the development of Robotics Application such as Rovers. Our recent accomplishment includes showcasing our innovative Infinit-Rover 2 in front of Prime Minister Shri Narendra Modi and Education Minister Dharmendra Pradhan during the Valedictory Ceremony of Centenary Celebration of University of Delhi and now we are aiming to move a step ahead and participate in International Rover Competitions with our Upgraded Infinit-Rover 3.
+    I hope this email finds you well. My name is Ravikant Verma, and I am writing on behalf of the Robotics Society of Kirori Mal College, Delhi University. We work on robotics applications in which we make various minor and major projects. As a major project we have built multiple Rover Prototypes. Recently we presented our Mars Rover Prototype Infinit Rover 2 in front of our honourable Prime Minister and Education Minister, in the Centenary Celebration Ceremony of Delhi University.
 
-Our Rover Project involves designing and building a cutting-edge rover to compete in international challenges. This initiative not only enhances the skills of our student members but also contributes to the overall advancement of science and technology.
+    Currently, we are the only Robotics Society in University of Delhi which works on Rovers and we aim to change this. Our vision is to initiate a technological and innovative stream in the field of theoretical physics.
+    For the same our next step is to participate in Australian Rover Challenge 2024 which will be held in the month of March 2024 and we have successfully qualified the CDR round of the competition. 
 
-We are reaching out to you because we believe {} shares our commitment to fostering innovation and education. The Rover Project aligns with our mission to push the boundaries of technological advancements and inspire future generations. To achieve our goals, we are seeking financial support and sponsorship from organizations that recognize the value of investing in education and technological innovation.
-
-We understand the importance of a symbiotic relationship, and we are open to discussing customized sponsorship packages that align with your corporate objectives.
-
-If you are interested in supporting our project, we would be grateful to arrange a meeting at your convenience to further discuss the details. We are confident that this partnership will not only contribute to the success of our Rover Project but also showcase your company as a supporter of innovation and education.
+    We are reaching out to you because we believe {} shares our commitment to fostering innovation and education. 
+    The Rover Project aligns with our mission to push the boundaries of technological advancements and inspire future generations. To achieve our goals, we are seeking financial support in the form of sponsorship from organizations that recognize the value of investing in education and technological innovation.
 
 
-Best Regards,
+    We understand the importance of a symbiotic relationship, and we are open to discussing customized sponsorship packages that align with your corporate objectives.
 
-Ravikant Verma
-Project Chief
-Robotics Society of Kirori Mal College, University of Delhi
-+91 788 074 9927
-""".format(i.name,i.company)
+    If you are interested in supporting our project, we would be grateful to arrange a meeting at your convenience to further discuss the details. We are confident that this partnership will not only contribute to the success of our Rover Project but also showcase your company as a supporter of innovation and education.
+
+
+    Best Regards,
+
+    Ravikant Verma
+    Project Chief
+    Robotics Society of Kirori Mal College, University of Delhi
+    +91 788 074 9927
+    """.format(i.name,i.company)
 
     em.attach(MIMEText(body, 'plain'))
 
-    
     pdf_filename1 = 'Infinit-Rover 3 Proposal.pdf'
     with open(pdf_filename1, 'rb') as attachment1:
         part1 = MIMEApplication(attachment1.read(), Name='Infinit-Rover 3 Proposal.pdf')
